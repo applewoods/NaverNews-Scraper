513318f63cabee34a7dba99ea49f2db41b7f1b8a
Woomin Nam <applewoods@g.skku.edu>
2021-01-25 14:47:43 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,11 @@ setup(
     name = naver_news_scraper_lib,
     version = "1.0.0",
     description = "woo's naver news scrap",
-    url = ""
+    url = "https://github.com/applewoods/NaverNews-Scraper.git",
+    author = "applewoods",
+    install_requires = [
+        'pandas==1.1.3',
+        'beautifulsoup4==4.4.1',
+        'requests==2.24.0'
+    ]
 )
